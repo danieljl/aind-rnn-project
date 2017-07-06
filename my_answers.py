48ebd7dbda7af2a8cb0437adfba0b0a446082364
Daniel Julius Lasiman <dj.lasiman@gmail.com>
2017-07-06 09:05:26 +0700

    Implement "TODO #2: Create a simple RNN model using keras to perform regression"

--- my_answers.py
+++ my_answers.py
@@ -25,9 +25,14 @@ def window_transform_series(series, window_size):
 
     return X,y
 
+
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential([
+        LSTM(units=5, input_shape=(window_size, 1)),
+        Dense(units=1),
+    ])
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
